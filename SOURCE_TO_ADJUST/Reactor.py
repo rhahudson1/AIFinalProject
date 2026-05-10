e7aad57bddc81c26eb7e934748478da496716693
Ivan Mendez Beltran <100553927@alumnos.uc3m.es>
2026-05-10 15:43:19 +0200

    implementation of reactor.py functions

--- SOURCE_TO_ADJUST/Reactor.py
+++ SOURCE_TO_ADJUST/Reactor.py
@@ -31,20 +31,30 @@ class Reactor:
 
     def compute_max_power(self) -> np.float64:
         """ Computes the maximum power of a reactor based on its physical features """
-        ### TO BE COMPLETED BY THE STUDENTS ###
-        ...
+        # P_max = Sigma_f * Flux * Volume * Energy_per_fission
+        return np.float64(self.effective_section * self.neutron_flux * self.core_volume * self.fision_energy)
     
     def compute_k(self) -> np.float64:
         """ Computes the value of the k-constant """
-        ### TO BE COMPLETED BY THE STUDENTS ###
-        ...
+        # The constant k is defined so that at maximum insertion (B=1) the power is 1 Watt
+        # k = ln(P_max)
+        return np.float64(np.log(self.max_power))
     
     def compute_power(self, control_bars_insertion: np.float64) -> np.float64:
         """ Computes the power delivered (%) by the reactor based on the % of control-bars inserted """
-        ### TO BE COMPLETED BY THE STUDENTS ###
-        ...
+        # P = P_max * exp(-k * B)
+        # We return the percentage relative to max power (0.0 to 1.0)
+        p_watts = self.max_power * np.exp(-self.k * control_bars_insertion)
+        return np.float64(p_watts / self.max_power)
     
     def compute_control_bars_insertion(self, power: np.float64) -> np.float64:
         """ Computes the % of controls-bars inserted based on the % of power delivered by the reactor """
-        ### TO BE COMPLETED BY THE STUDENTS ###
-        ...
+        # This is the inverse of the power formula. If P = P_max * exp(-k*B)
+        # Then B = -ln(P_actual / P_max) / k
+        # Since 'power' passed here is already the percentage (0.0 to 1.0),
+        # P_actual / P_max is simply the 'power' variable.
+
+        # Protect against log(0)
+        p_safe = max(power, 1e-12)
+        insertion = -np.log(p_safe) / self.k
+        return np.float64(np.clip(insertion, 0.0, 1.0))
